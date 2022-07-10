102406e87cd2dd94f9ae4b90e5f0470574a80f75
Lucas Martins <a.lucas460@gmail.com>
2022-07-10 16:40:40 -0300

    Corrigindo rotas

--- api/Data.py
+++ api/Data.py
@@ -1,19 +1,19 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-def getdata():
+def getData():
     #atualizar a cada chamada na outra api  
     y,x= np.loadtxt('gait.txt',delimiter =',', usecols =(0, 1),unpack = True)
     return list(x),list(y)
 
 def FurrierTransform():
     #atualizar a cada chamada na outra api  
-    x,y= getdata()
+    x,y= getData()
     result = x,y
     return result
 
 def WaveletTrasnform():
     #atualizar a cada chamada na outra api  
-    x,y= getdata()
+    x,y= getData()
     result = x,y
     return result
